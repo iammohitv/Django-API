1529ed1a652d9479a7b4f81aa0946a39da0c301d
mohit <mohit.vaswani.work@gmail.com>
2023-11-23 13:53:53 -0500

    ADDED: a basic model and ran throught serialization

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,15 @@
 import json
 from django.http import JsonResponse
 
-def api_home(request,*args,**kwargs):
-    body = request.body
-    data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    # print(data)
-    print(request.GET)
-    print(request.POST)
-    # print(request.headers)
-    # data['headers'] = dict(request.headers)
-    data['params'] = request.GET
-    data['content_type'] = request.content_type
+from products.models import Product
 
-    print(data)
+def api_home(request,*args,**kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint, params={'abc':123},json={"query":"Hello King!"}) #API -> method
+response = requests.get(endpoint, params={'abc':123}) #API -> method
 
 print(response.json())
