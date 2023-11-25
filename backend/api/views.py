2f94bb5bf507dd7a6f918e81fa0b82143e0be6d4
mohit <mohit.vaswani.work@gmail.com>
2023-11-25 14:12:13 -0500

    ADDED more things on serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,31 @@
 import json
-from django.http import JsonResponse
-
+# from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from products.models import Product
+from products.serializers import ProductSerializer
 
+@api_view(["POST"])
 def api_home(request,*args,**kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+    """
+    DRF API VIEW
+    """
+    data = request.data
+    # model_data = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if model_data:
+    #     data['id'] = model_data.id
+    #     data['title'] = model_data.title
+    #     data['content'] = model_data.content
+    #     data['price'] = model_data.price
+    # if model_data:
+        # data = model_to_dict(model_data,fields = ['id','price', 'title', 'sale_price'])
+    serializer = ProductSerializer(data=request.data)
 
-    return JsonResponse(data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        
+        # print(instance)
+        data = serializer.data
+        return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint, params={'abc':123}) #API -> method
+response = requests.post(endpoint, json={'Title':'king'}) #API -> method
 
 print(response.json())
